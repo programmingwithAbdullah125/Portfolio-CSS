da0387ecfdeb169b4c282b9f3e825c5d3aa41465
Abdullah <abdullahateeq331@gmail.com>
2024-11-10 23:54:25 +0500

    my first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import './aboutPage.css'; 
+export default function About() {
+    return (
+      <div className="container">
+        <div className="about">
+        <h1>About Me</h1>
+        <p>I am Abdullah</p>
+        <p>I am in FSc-II and learning artificial intelligence</p>
+        <p>I am a frontend developer and leveling up more skills in the field of IT</p>
+        <p>I am studying in Governor Initiative for Artificial Intelligence and Computing.</p>
+      </div>
+      </div>
+    )
+  }
+  
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import './contactPage.css'
+export default function Contact() {
+    return (
+      <div className="container">
+      <div className="contact-page">
+        <h1 className="heading">Contact Us</h1>
+        <p>Email: abdullahateeq331@gmail.com</p>
+        <p>Phone: 03362310043</p>
+      </div>
+      </div>
+    );
+  }
+  
